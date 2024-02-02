9e49dee3aa23eb66ba8c4520e278bdf3427bb6b0
Murendenimudzusi <113940379+Murendenimudzusi@users.noreply.github.com>
2024-02-02 15:19:26 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     String [] list = {"the", "quick", "brown", "fox", "ate", "my", "chickens" };
+//
+        System.out.printin( String [] list);
+
+    }
+      static  void longest ( String [] list){
+
+          
+
+
+          for (int i = 1; i < strings.length; i++) {
+              String str = strings[0];
+              if (strings[i].length() > str.length()) {
+                  str = strings[i];
+              }
+          }
+
+
+
+        return; str
+
+      }
+
+}
